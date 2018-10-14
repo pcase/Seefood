da8f4a0fe6dd25c2b9122404b5a55e36e3bf3e5b
Patty Case <beadypatty@yahoo.com>
2018-10-14 13:38:31 -0700

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Patty Case on 10/14/18.
+//  Copyright © 2018 Azure Horse Creations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
